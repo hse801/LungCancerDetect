3710b5cf8d571b23daf693a021d535e358617cce
hse801 <sarah0.hwang@gmail.com>
2021-03-30 15:14:55 +0900

    label the lymph node data

--- lungcancer/copy_lymph_out.py
+++ lungcancer/copy_lymph_out.py
@@ -6,6 +6,7 @@ import shutil
 # copy2(src, dst): copy file with meta data
 # copy file out of the RoiVolume_cut folder
 
+
 def copy_file_out(file_path):
 
     os.chdir(file_path)
@@ -18,8 +19,9 @@ def copy_file_out(file_path):
             shutil.copy2(lymph_roi_path + f, file_path)
         print('file copied from ', lymph_roi_path, ' to ', file_path)
 
-
-file_path = glob.glob('E:/HSE/LungCancerDetect/data/testset/*/')
+file_path = glob.glob('E:/HSE/LungCancerDetect/data/images/train/*/')
+# file_path = glob.glob('E:/HSE/LungCancerDetect/data/images/valid/*/')
+# file_path = glob.glob('E:/HSE/LungCancerDetect/data/testset/*/')
 
 for i in file_path:
     copy_file_out(i)
--- lungcancer/lymph_yolo_label.py
+++ lungcancer/lymph_yolo_label.py
@@ -29,6 +29,8 @@ import SimpleITK as sitk
 # 2d data labeling을 위해 각 roi 영역의 xcenter, ycenter, w, h를 txt 파일로 저장
 # 기존의 roi가 한줄씩 표시된 라벨 데이터에 lymph node 데이터가 존재한다면 추가적으로 라벨링 해준다.
 # class 2개
+# ROI_cut.nii.gz 0개 이상 1개 이하
+# *_lymph_cut.nii.gz 0개 이상
 
 
 def get_lymph_label(fPath):
@@ -36,35 +38,31 @@ def get_lymph_label(fPath):
     roi_list = glob.glob(fPath + 'ROI_cut.nii.gz')
     lymph_list = glob.glob(fPath + '*_lymph_cut.nii.gz')
 
-    for l in lymph_list:
-        img_lymph = sitk.ReadImage(lymph_list[l])
-        img_lymph_data = sitk.GetArrayFromImage(img_lymph)
-        nzero = img_lymph_data.nonzero()
+    # if roi_cut exist
+    if len(roi_list) >= 1:
+        print('ROI_cut exists in ', fPath)
+        img_roi = sitk.ReadImage(roi_list[0])
+        img_roi_data = sitk.GetArrayFromImage(img_roi)
+        nzero = img_roi_data.nonzero()
         new_nzero_z = []
         for i in nzero[0]:
             if i not in new_nzero_z:
                 new_nzero_z.append(i)
-        print('new nonzero = ', new_nzero_z)
+        # print('new nonzero = ', new_nzero_z)
 
         start_idx = new_nzero_z[0]
         end_idx = new_nzero_z[-1]
-
-        print('start idx = ', start_idx)
-        print('end idx = ', end_idx)
-
-        z_count = 0
-
-        for i in range(np.shape(img_lymph_data)[0]):
-            if start_idx > i or end_idx < i:
-                os.chdir(fPath)
-                num = '{0:0>3}'.format(i)
-                with open("CT_PET_slice" + str(num) + ".txt", "w") as f:
-                    pass
+        print(f'start idx = {start_idx}, end idx = {end_idx}')
+        for i in range(np.shape(img_roi_data)[0]):
+            # if start_idx > i or end_idx < i:
+            #     os.chdir(fPath)
+            #     num = '{0:0>3}'.format(i)
+            #     with open("CT_PET_slice" + str(num) + ".txt", "w") as f:
+            #         pass
             if start_idx <= i <= end_idx:
-                roi_zslice = img_lymph_data[i, :, :]
-                # print('i1 = ', i)
+                roi_zslice = img_roi_data[i, :, :]
                 nzero = roi_zslice.nonzero()
-                print('nzero = ', nzero)
+                # print('nzero = ', nzero)
                 new_nzero_x = []
                 new_nzero_y = []
                 for j in nzero[1]:
@@ -73,57 +71,117 @@ def get_lymph_label(fPath):
                 for k in nzero[0]:
                     if k not in new_nzero_y:
                         new_nzero_y.append(k)
-                z_count += 1
                 # normalize with image width and height (160x128)
-
                 centerX = ((min(new_nzero_x) + max(new_nzero_x)) / 2)/160
                 centerY = 1 - (((min(new_nzero_y) + max(new_nzero_y)) / 2)/128)
                 # centerY = 1 - centerY
                 w = (max(new_nzero_x) - min(new_nzero_x))/160
                 h = (max(new_nzero_y) - min(new_nzero_y))/128
-
-                # print('center X = ', centerX)
-                # print('certerY = ', centerY)
-                # print('w = ', w)
-                # print('h = ', h)
-                # print('i = ', i)
                 os.chdir(fPath)
                 num = '{0:0>3}'.format(i)
-                print('num = ', num)
+                # print('num = ', num)
 
-                # "a" is append mode
-                # 기존의 파일의 마지막에 추가
-                with open("CT_PET_slice" + str(num) + ".txt", "a") as f:
-                    f.write("1 " + str(centerX) + " ")
+                with open("CT_PET_slice" + str(num) + ".txt", "w") as f:
+                    f.write("0 " + str(centerX) + " ")
                     f.write(str(centerY) + " ")
                     f.write(str(w) + " ")
-                    f.write(str(h) + " ")
-                # with open("CT-cut-slice" + str(num) + ".txt", "w") as f:
-                #     f.write("0 " + str(centerX) + " ")
-                #     f.write(str(centerY) + " ")
-                #     f.write(str(w) + " ")
-                #     f.write(str(h) + " ")
-                # with open("PET-cut-slice" + str(num) + ".txt", "w") as f:
-                #     f.write("0 " + str(centerX) + " ")
-                #     f.write(str(centerY) + " ")
-                #     f.write(str(w) + " ")
-                #     f.write(str(h) + " ")
+                    f.write(str(h) + " " + "\n")
+                print('Have ROI_cut and in roi')
             # roi에 해당하지 않는 z 인덱스의 경우 label 없는 빈 txt 파일 생성
             else:
                 os.chdir(fPath)
                 num = '{0:0>3}'.format(i)
-                with open("CT-cut-slice" + str(num) + ".txt", "w") as f:
+                with open("CT_PET_slice" + str(num) + ".txt", "w") as f:
                     pass
+                # print("Have ROI_cut but not roi")
+    # elif len(roi_list) == 0:
+    #     for j in range(80):
+    #         num = '{0:0>3}'.format(j)
+    #         with open("CT_PET_slice" + str(num) + ".txt", "w") as f:
+    #             pass
+    #     print('Do not have ROI_cut')
+
+    # if lymph roi file exist
+    if lymph_list:
+        print(f'lymph list = {lymph_list}')
+        for l in lymph_list:
+            img_lymph = sitk.ReadImage(l)
+            img_lymph_data = sitk.GetArrayFromImage(img_lymph)
+            nzero = img_lymph_data.nonzero()
+            # print(f'nzero = {nzero}, type of nzero = {type(nzero)}')
+            new_nzero_z = []
+            for i in nzero[0]:
+                if i not in new_nzero_z:
+                    new_nzero_z.append(i)
+            # print(f'new nzero = {new_nzero_z}, type of nzero = {type(new_nzero_z)}')
+            # If ROI data is empty
+            if new_nzero_z == []:
+                print('ROI is empty')
+                continue
+            # print('new nonzero = ', new_nzero_z)
+
+            start_idx = new_nzero_z[0]
+            end_idx = new_nzero_z[-1]
+            print(f'start idx = {start_idx}, end idx = {end_idx}')
+
+            for i in range(np.shape(img_lymph_data)[0]):
+                # if start_idx > i or end_idx < i:
+                #     os.chdir(fPath)
+                #     num = '{0:0>3}'.format(i)
+                #     with open("CT_PET_slice" + str(num) + ".txt", "a") as f:
+                #         pass
+                if start_idx <= i <= end_idx:
+                    roi_zslice = img_lymph_data[i, :, :]
+                    # print('i1 = ', i)
+                    nzero = roi_zslice.nonzero()
+                    # print('nzero = ', nzero)
+                    new_nzero_x = []
+                    new_nzero_y = []
+                    for j in nzero[1]:
+                        if j not in new_nzero_x:
+                            new_nzero_x.append(j)
+                    for k in nzero[0]:
+                        if k not in new_nzero_y:
+                            new_nzero_y.append(k)
+                    # normalize with image width and height (160x128)
+
+                    centerX = ((min(new_nzero_x) + max(new_nzero_x)) / 2)/160
+                    centerY = 1 - (((min(new_nzero_y) + max(new_nzero_y)) / 2)/128)
+                    # centerY = 1 - centerY
+                    w = (max(new_nzero_x) - min(new_nzero_x))/160
+                    h = (max(new_nzero_y) - min(new_nzero_y))/128
+
+                    os.chdir(fPath)
+                    num = '{0:0>3}'.format(i)
+
+                    # "a" is append mode
+                    # 기존의 파일의 마지막에 추가
+                    with open("CT_PET_slice" + str(num) + ".txt", "a") as f:
+                        f.write("1 " + str(centerX) + " ")
+                        f.write(str(centerY) + " ")
+                        f.write(str(w) + " ")
+                        f.write(str(h) + " " + "\n")
+                    print('Patient ', l, ' Have lymph node')
+
+    # os.chdir(fPath)
+    # for i in range(80):
+    #     num = '{0:0>3}'.format(i)
+    #     if not os.path.isfile(fPath + "CT_PET_slice" + str(num) + ".txt"):
+    #         with open("CT_PET_slice" + str(num) + ".txt", "w") as f:
+    #             pass
+    #         print('Create label for empty images')
+
 
 
 # foldList = glob.glob('E:/HSE/LungCancerDetect/data/testset/*/')
-# foldList = glob.glob('E:/HSE/LungCancerDetect/data/images/train/*/')
-# foldList = glob.glob('E:/HSE/LungCancerDetect/data/images/valid/*/')
-foldList = glob.glob('E:/HSE/LungCancerDetect/one/23835418/')
+foldList = glob.glob('E:/HSE/LungCancerDetect/data/images/train/*/')
+# foldList = glob.glob('E:/HSE/LungCancerDetect/data/imags/valid/*/')
+# foldList = glob.glob('E:/HSE/LungCancerDetect/one/23835418/')
 count = 0
 
 for i in foldList:
     get_lymph_label(i)
     count += 1
     print('count = ', count)
+    # break
 
